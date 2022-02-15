0302f88603dfb7756889bf82c3fa0157f29e04c7
Sungwoo Park <tjddneva@naver.com>
2022-02-16 01:00:35 +0900

    waitkey

--- ch01/HelloCV.py
+++ ch01/HelloCV.py
@@ -11,8 +11,14 @@ if img is None:
 
 cv2.imwrite('cat_gray.png', img)
 
+# 없어도 imshow 가 자동으로 창 만듬 but 무조건 WINDOW_AUTOSIZE 로 설정됨
 cv2.namedWindow('image')
-cv2.imshow('image', img)
-cv2.waitKey()
 
-cv2.destroyAllWindows()
+cv2.imshow('image', img)
+# cv2.waitKey()
+while True:
+    if cv2.waitKey() == ord('q'):
+        break
+
+# cv2.destroyAllWindows()
+cv2.destroyWindow('image')  # 없어도 window 가 자동으로 회수
